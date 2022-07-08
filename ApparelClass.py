e175f17f69af7cdb7f1ec70190dc5d01880ddd81
Ajinkya Pradhan <101694262+AjinkyaPradhan1@users.noreply.github.com>
2022-07-08 12:21:15 +0530

    Add files via upload

--- ApparelClass.py
+++ ApparelClass.py
@@ -1,18 +1,14 @@
 class Apparel:
-    def __init__(self,item_id,price,item_type):
-        self.__item_id = ['C101','C102','C103','C104','S101','S102','S103','S104']
+    counter  = 0
+    def __init__(self,price,item_type):
+        Apparel.counter += 1
+        self.__item_id = item_type[0]+str(Apparel.counter)
         self.__price = price
         self.__item_type = item_type
-        self.counter = 100
-
-    def set_item_id(self,item_id):
-        self.__item_id = item_id
+        
 
     def set_price(self,price):
         self.__price = price
-
-    def set_item_type(self,item_type):
-        self.__item_type = item_type
 
     def get_item_id(self):
         return self.__item_id
@@ -24,7 +20,7 @@ class Apparel:
         return self.__item_type
 
     def calculate_price(self):
-        self.__price *= (0.05*self.__price)
+        self.__price += 0.05*self.__price
         return self.__price
 
 class Cotton(Apparel):
@@ -35,24 +31,26 @@ class Cotton(Apparel):
     def calculate_price(self):
         super().calculate_price()
         price = self.get_price()
-        price -= price*(0.1*self.get_price())
+        price -= price*(self._discount/100)
+        price += price*0.05
+
+    def get_discount(self):
+        return self.__discount
 
 
 class Silk(Apparel):
-    def __init__(self,price,discount,points):
+    def __init__(self,price):
         super.__init__(price,"Silk")
-        self.__discount = discount
-        self.__points = points
+        self.__points = None
+        
 
     def calculate_price(self):
         super().calculate_price()
-        price = self.get_price()
-        price -= price*(0.1*self.get_price())
-
-        if(self.__price>10000):
+        if(self.get_price()>10000):
             self.__points = 10
         else:
             self.__points = 3
+        return self.get_price + (self.get_price()* (0.1)) 
     
-    def get_points(self,get_points):
-        return self.__get_points
+    def get_points(self):
+        return self.__points
